b05764d4c9abb662867c84db6383793781af6c8d
OYXJ <OYXJlucky@163.com>
2016-12-20 23:28:35 +0800

    LuckyDrawViewController.h 仿微信webview，继承HTmWebViewController

---
+++ WeChatWebViewController/TEST/Web/LuckyDrawViewController.h
@@ -0,0 +1,14 @@
+//
+//  LuckyDrawViewController.h
+//  TemplatesProject
+//
+//  Created by HawkingOuYang.com on 15/12/1.
+//  Copyright © 2015年 [OYXJlucky@163.com] . All rights reserved.
+//
+
+#import "HTmWebViewController.h"
+
+//! 仿微信webview，继承HTmWebViewController
+@interface LuckyDrawViewController : HTmWebViewController
+
+@end
